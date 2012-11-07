7fe6a4a2de751591fbe9f6bfc26fc1afb7ab4e9a
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-06 19:31:46 -0500

    Tweaked alias for fork to patcon account. Created build matrix for drush versions.

--- tests/behat/classic.aliases.drushrc.php
+++ tests/behat/classic.aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['classic.local'] = array(
   'uri' => '127.0.0.1',
-  'root' => '/home/travis/builds/sprice/drupal',
+  'root' => '/home/travis/builds/patcon/drupal',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
